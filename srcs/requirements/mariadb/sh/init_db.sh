70ca7465f10ddecaa98d0b281098c2d80944f5d7
Nicolas Patron <nicolas.patron@hotmail.fr>
2024-08-07 15:50:49 +0200

    better scripts, need to work now on VM to change var in etc/

--- srcs/requirements/mariadb/sh/init_db.sh
+++ srcs/requirements/mariadb/sh/init_db.sh
@@ -1,46 +1,27 @@
 #!/bin/bash
 # 
 
-# mkdir -p /etc/sh/myErrorFile
-# myErrorFile="/etc/sh/myErrorFile/error.log"
-# mkdir -p /etc/sh/mySuccessFile
-# mySuccessFile="/etc/sh/mySuccessFile/success.log"
-
-# touch "$myErrorFile"
-# touch "$mySuccessFile"
-
-# chmod 664 "$myErrorFile"
-# chmod 664 "$mySuccessFile"
-
-# if [ -z "$MYSQL_DATABASE" ]; then
-#     mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
-#     echo "Initialization of $MYSQL_DATABASE..." >> "$mySuccessFile"
-
-#     if [ -z "$MYSQL_PASSWORD" ]; then
-#         mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_DATABASE}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-#         echo "Giving privileges on $MYSQL_DATABASE to $MYSQL_DATABASE..." >> "$mySuccessFile"
-#     else
-#         echo "'MYSQL_PASSWORD' variable is empty" >> "$myErrorFile"
-#     fi
-# else
-#     echo "'MYSQL_DATABASE' variable is empty" >> "$myErrorFile"
-# fi
-
-# if [ -z "$MYSQL_ROOT_PASSWORD" ]; then
-#     mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-#     mysql -e "FLUSH PRIVILEGES;"
-#     mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
-# else
-#     echo "'MYSQL_ROOT_PASSWORD' variable is empty" >> "$myErrorFile"
-# fi
 
 service mysql start
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${MARIADB_USER}\`@'localhost' IDENTIFIED BY '${MARIADB_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
-mysql -p"$MARIADB_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
-echo "5"
 
-pkill mysqld
-# mysqladmin -u root -p"$MARIADB_ROOT_PASSWORD" shutdown
+if [ -n "$DB_NAME" ]; then
+	mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+    echo "Initialisation of $DB_NAME..."
+	if [ -n "$DB_USER" ] && [ -n "$DB_PASSWORD" ]; then
+		echo "Registration of one USER into the DB..."
+		mysql -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
+		echo "Giving MariaDB's privileges to ${DB_USER}"
+		mysql -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+    fi
+fi
+
+if [ -n "$DB_ROOT_PASSWORD" ]; then
+    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
+	echo "Refreshing new settings..."
+    mysql -p"$DB_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
+	echo "Restart of MySQL, changes will take effect..."
+    mysqladmin -u root -p"$DB_ROOT_PASSWORD" shutdown
+fi
+
+exec mysqld_safe
+
--- srcs/requirements/wordpress/conf/config_wp.sh
+++ srcs/requirements/wordpress/conf/config_wp.sh
@@ -1,15 +1,22 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    config_wp.sh                                       :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: npatron <npatron@student.42.fr>            +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/08/07 14:57:52 by npatron           #+#    #+#              #
+#    Updated: 2024/08/07 14:58:06 by npatron          ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 #!/bin/bash
-sleep 10
-wp config create	--allow-root \
-                    --dbname=$MYSQL_DATABASE \
-                    --dbuser=$MYSQL_USER \
-                    --dbpass=$MYSQL_PASSWORD \
-                    --dbhost=mariadb:3306 --path='/var/www/wordpress'
 
-wp core install --url="https://$MYSERVER_NAME" --title="Le site du SIECLE" \
-                --admin_user="NICOLAS PATRON" --admin_password="mypassword" \
-                --admin_email="nicolas.patron@hotmail.fr"
+echo "clear_env = no" >> /etc/php/7.3/fpm/pool.d/www.conf
+sed -i 's#listen = /run/php/php7.3-fpm.sock#listen=wordpress:9000#' \ 
+        /etc/php/7.3/fpm/pool.d/www.conf
 
-wp user create  ornoux ornaxgame@gmail.com --role=user --user_pass=pass \
-                --first_name="Ornax" --last_name="Valorant" --display_name="LeBgDu06"
-                
+sed -i "s/define( 'DB_NAME', 'votre_nom_de_bdd' );/define( 'DB_NAME', '$DB_NAME' );/" /var/www/wordpress/wp-config-sample.php
+sed -i "s/define( 'DB_USER', 'votre_utilisateur_de_bdd' );/define( 'DB_USER', '$DB_USER' );/" /var/www/wordpress/wp-config-sample.php
+sed -i "s/define( 'DB_PASSWORD', 'votre_mdp_de_bdd' );/define( 'DB_PASSWORD', '$DB_PASSWORD' );/" /var/www/wordpress/wp-config-sample.php
+
