b030c7927bff9e1e70226bf6b0dd436714554534
Your Name <you@example.com>
2024-08-08 09:16:16 +0200

    mariadb fix, need to fix wordpress

--- srcs/requirements/mariadb/sh/init_db.sh
+++ srcs/requirements/mariadb/sh/init_db.sh
@@ -1,40 +1,34 @@
 #!/bin/bash
 
+chown -R mysql:mysql /var/lib/mysql
+
+mysql_install_db --user=mysql --datadir=/var/lib/mysql
+
+/usr/bin/mysqld_safe --datadir=/var/lib/mysql &
+
+while true; do
+    if /usr/bin/mysqladmin ping --silent; then
+        echo "MySQL is online baby"
+        break
+    fi
+    sleep 1
+done
 
 
-# service mysql start
+if [ -n "$DB_NAME" ]; then
+    echo "Checking if $DB_NAME already exists..."
+    mysql -u root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
+    if [ -n "$DB_USER" ] && [ -n "$DB_PASSWORD" ]; then
+        echo "Checking if $DB_USER already exists..."
+        mysql -u root -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
+        echo "Giving to $DB_USER privileges from $DB_NAME"
+        mysql -u root -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+        echo "Refreshing settings...changements will take effect..."
+        mysql -u root -e "FLUSH PRIVILEGES;"
+    fi
+fi
 
-# if [ -n "$DB_NAME" ]; then
-# 	mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
-#     echo "Initialisation of $DB_NAME..."
-# 	if [ -n "$DB_USER" ] && [ -n "$DB_PASSWORD" ]; then
-# 		echo "Registration of one USER into the DB..."
-# 		mysql -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
-# 		echo "Giving MariaDB's privileges to ${DB_USER}"
-# 		mysql -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
-#     fi
-# fi
+mysqladmin shutdown
 
-# if [ -n "$DB_ROOT_PASSWORD" ]; then
-#     mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
-# 	echo "Refreshing new settings..."
-#     mysql -p"$DB_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
-# 	echo "Restart of MySQL, changes will take effect..."
-#     mysqladmin -u root -p"$DB_ROOT_PASSWORD" shutdown
-# fi
-
-# exec mysqld_safe
-
-service mysql start
-
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
-mysqladmin -u root password $DB_ROOT_PASSWORD
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
-mysql -p"$DB_ROOT_PASSWORD" -e "FLUSH PRIVILEGES;"
-mysqladmin -u root -p$DB_ROOT_PASSWORD shutdown
-exec mysqld_safe
-
-
+/usr/bin/mysqld_safe --datadir=/var/lib/mysql
 
